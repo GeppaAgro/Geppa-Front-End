5eea87f51ef459f89d7af4db75d35da7fcfe251f
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-01-22 23:35:03 -0300

    feat: Adicionado novo componente de header

--- geppa-front-end/src/Presentation/Components/Header/Header.tsx
+++ geppa-front-end/src/Presentation/Components/Header/Header.tsx
@@ -1,23 +1,51 @@
-import "./StyleHeader.css"
-import { NavLink} from "react-router-dom";
+import LogoHorizontal from "../../../Data/Images/Logos/LogoHorizontal.png";
+import "./header.css";
+import {NavLink} from "react-router-dom";
+import {Image} from "react-bootstrap";
 
-
-
-export default function Header() {
-    return(
+const Header = () => {
+    return (
         <>
-            <header className="header">
-                <img className="headerlogo" src="src/Data/Images/Logos/LogoHorizontal.png" alt="Logo_Horizontal"/>
-                <nav className="headernavigation">
-                    <ul className="headernavigation-list">
-                        <li className="headernavigation-item"><NavLink to="/">Inicial</NavLink></li>
-                        <li className="headernavigation-item"><NavLink to="/boletins">Boletins</NavLink></li>
-                        <li className="headernavigation-item"><NavLink to="/conteudos">Conteúdos</NavLink></li>
-                        <li className="headernavigation-item"><NavLink to="/indicadores">Indicadores</NavLink></li>
-                        <li className="headernavigation-item"><NavLink to="/sobre">Sobre</NavLink></li>
-                    </ul>
-                </nav>
-            </header>
+            <nav className="navbar navbar-expand-lg bg-white shadow-sm">
+                <div className="container-fluid px-5">
+                    <div className="d-flex justify-content-between">
+                        <NavLink to={"/"}>
+                            <Image className="navbar-brand img-fluid nav-img" role="button"
+                                   src={LogoHorizontal}/>
+                        </NavLink>
+                        <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon botao"/>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarScroll">
+                        <ul className="navbar-nav fs-5 navbar-nav-scroll gap-2 align-items-center">
+                            <li className="nav-link teste" aria-current="page">
+                                <NavLink className="text-decoration-none nav-link-font" to="/">Inicial</NavLink>
+                            </li>
+                            <li className="nav-link" aria-current="page">
+                                <NavLink className="text-decoration-none nav-link-font"
+                                         to="/boletins">Boletins</NavLink>
+                            </li>
+                            <li className="nav-link" aria-current="page">
+                                <NavLink className="text-decoration-none nav-link-font"
+                                         to="/conteudos">Conteúdos</NavLink>
+                            </li>
+                            <li className="nav-link" aria-current="page">
+                                <NavLink className="text-decoration-none nav-link-font"
+                                         to="/indicadores">Indicadores</NavLink>
+                            </li>
+                            <li className="nav-link " aria-current="page">
+                                <NavLink className="text-decoration-none nav-link-font"
+                                         to="/sobre">Sobre</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
+
+export default Header;
