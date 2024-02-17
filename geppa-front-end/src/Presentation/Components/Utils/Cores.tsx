1f654a95855f9179f5a3765b3a89c4ad00888ddb
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-02-16 23:11:28 -0300

    feat: adicionado constante de cores

---
+++ geppa-front-end/src/Presentation/Components/Utils/Cores.tsx
@@ -0,0 +1,14 @@
+const Cores = {
+    verdeOliva: '#556B2F',
+    verdeCampo: '#4CAF50',
+    azulCeu: '#87CEEB',
+    amareloGirassol: '#FFD700',
+    laranjaSol: '#FFA500',
+    marromChocolate: '#D2691E',
+    marromEscuro: '#614848',
+    marromTerra: '#8D6E63',
+    branco: '#FFFFFF',
+    brancoTransparente: '#FFFFFF99',
+};
+
+export default Cores;
