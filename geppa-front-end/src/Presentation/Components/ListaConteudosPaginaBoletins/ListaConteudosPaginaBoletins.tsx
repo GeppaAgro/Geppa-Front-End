7da5afd97e4fd62fcc79049c8a2013d1d1d515ca
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-04-15 21:50:32 -0300

    feat: Adicionado loading com skeleton para os ultimos conteudos simplificados

--- geppa-front-end/src/Presentation/Components/ListaConteudosPaginaBoletins/ListaConteudosPaginaBoletins.tsx
+++ geppa-front-end/src/Presentation/Components/ListaConteudosPaginaBoletins/ListaConteudosPaginaBoletins.tsx
@@ -2,45 +2,64 @@ import "./StyleListaCOnteudosPaginaBoletim.css"
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import AxiosClient from "../../../Data/Services/AxiosClient.ts";
+import {UltimosConteudosSimplificadoSkeleton} from "../Skeleton/UltimosConteudosSimplificadoSkeleton.tsx";
 
-interface Conteudo{
-    dataPublicacao:number;
+interface Conteudo {
+    dataPublicacao: number;
     id: number;
-    titulo:string;
-    descricao:string;
-    link:string;
+    titulo: string;
+    descricao: string;
+    link: string;
 }
-export default function ListaConteudosPaginaBoletins(){
-    const[conteudos,setConteudos] = useState<Conteudo[]>([]);
+
+export default function ListaConteudosPaginaBoletins() {
+    const sizeConteudos = 10
+
+    const [conteudos, setConteudos] = useState<Conteudo[]>([]);
+    const [loadingConteudos, setLoadingConteudos] = useState<boolean>(true);
+
 
     useEffect(() => {
-        const buscarUltimosConteudos = async() => {
-            try{
-                const response = await AxiosClient.get(`/conteudos?size=15`);
+        const buscarUltimosConteudos = async () => {
+            try {
+                const response = await AxiosClient.get(`/conteudos?size=${sizeConteudos}`);
                 setConteudos(response.data.dados)
-            }catch (error){
+                setLoadingConteudos(false);
+            } catch (error) {
                 console.error('Erro ao buscar conteudo', error)
+                setLoadingConteudos(false);
             }
         }
         buscarUltimosConteudos()
     }, []);
 
-    return(
+    return (
         <>
-            <h5 >Últimos conteúdos</h5>
-
-            {
-                conteudos.map(
-                    (conteudo) =>
-                        (
-                            <div key={conteudo.id} className="containerConteudosSimplificado">
-                                <Link to={conteudo.link} className="fs-6 fw-semibold">
-                                    {conteudo.titulo}
-                                </Link>
-                            </div>
+            <h5>Últimos conteúdos</h5>
+            {loadingConteudos ? (
+                <>
+                    {Array.from({length: sizeConteudos}).map((_, index) => (
+                        <div key={index} className="containerConteudosSimplificado">
+                            <UltimosConteudosSimplificadoSkeleton/>
+                        </div>
+                    ))}
+                </>
+            ) : (
+                <>
+                    {
+                        conteudos.map(
+                            (conteudo) =>
+                                (
+                                    <div key={conteudo.id} className="containerConteudosSimplificado">
+                                        <Link to={conteudo.link} className="fs-6 fw-semibold">
+                                            {conteudo.titulo}
+                                        </Link>
+                                    </div>
+                                )
                         )
-                )
-            }
+                    }
+                </>
+            )},
 
         </>
     )
