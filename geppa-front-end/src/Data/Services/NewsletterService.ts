20912456b7b5c3130eda0d9edac30d1a2b63a653
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-04-27 12:49:57 -0300

    refactor: refatorado a service newslleter para realizar a inscrição de novos email e tratar o retorno de sucesso e erro

--- geppa-front-end/src/Data/Services/NewsletterService.ts
+++ geppa-front-end/src/Data/Services/NewsletterService.ts
@@ -1,37 +1,50 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { Newsletter } from '../ApiTypes/TypeNewsletter.ts';
-import axiosClient from './AxiosClient.ts';
+import {AxiosError, AxiosResponse} from 'axios';
+import {Newsletter} from '../ApiTypes/TypeNewsletter.ts';
+import AxiosClient from "./AxiosClient.ts";
+
 
 export class NewsletterService {
-    private axiosClient: AxiosInstance;
-
-    constructor() {
-        this.axiosClient = axios.create({
-            baseURL: axiosClient.defaults.baseURL,
-        });
+    async inscricaoNewsletter(newsletter: Newsletter): Promise<{ success: boolean; message: string }> {
+        try {
+            const response: AxiosResponse = await AxiosClient.post('/newsletters/inscricao', newsletter);
+            return this.handleResponse(response);
+        } catch (error) {
+            const axiosError = error as AxiosError;
+            return this.handleError(axiosError);
+        }
     }
 
-    async inscricaoNewsletter(newsletter: Newsletter): Promise<boolean> {
-        try {
-            const response: AxiosResponse = await this.axiosClient.post('/newsletters/inscricao', { data: newsletter }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (response.status >= 200 && response.status < 300) {
-                return true;
-            } else {
-                throw new Error(`Failed to subscribe to newsletter. Status: ${response.status}`);
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error('Axios Error:', error.message);
-            } else {
-                console.error('Error:', error.message);
-            }
-
-            return false;
+    private handleResponse(response: AxiosResponse): { success: boolean; message: string } {
+        if (response.status >= 200 && response.status < 300) {
+            return {success: true, message: "Inscrição Realizada com sucesso!"};
+        } else {
+            return this.handleErrorResponse(response);
         }
+    }
+
+    private handleErrorResponse(response: AxiosResponse): { success: boolean; message: string } {
+        if (response.status === 400) {
+            if (response.data.mensagem === "Registro já cadastrado") {
+                return {success: false, message: "Este e-mail já está cadastrado na newsletter."};
+            } else if (response.data.errosValidacao) {
+                return {success: false, message: `${response.data.errosValidacao.email}`};
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Falha ao se inscrever na newsletter. Por favor, tente novamente mais tarde.'
+        };
+    }
+
+    private handleError(error: AxiosError): { success: boolean; message: string } {
+        if (error.response?.status === 400) {
+            return this.handleErrorResponse(error.response);
+        }
+
+        return {
+            success: false,
+            message: 'Ocorreu um erro ao se inscrever na newsletter. Por favor, tente novamente mais tarde.'
+        };
     }
 }
