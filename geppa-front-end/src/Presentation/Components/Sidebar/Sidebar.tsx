452221d31d1159f828c758bbbd3360972067b564
Luan Rodrigues <rodriguesluandev@gmail.com>
2024-04-27 22:13:04 -0300

    refactor: Sidebar Component

--- geppa-front-end/src/Presentation/Components/Sidebar/Sidebar.tsx
+++ geppa-front-end/src/Presentation/Components/Sidebar/Sidebar.tsx
@@ -1,41 +1,49 @@
-// Sidebar.tsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Image} from "react-bootstrap";
-import LogoHorizontal from "../../../Data/Images/Logos/LogoHorizontal.png";
+import { Image } from "react-bootstrap";
+import LogoHorizontalBranca from "../../../Data/Images/Logos/LogoHorizontalBranca.png";
+import './sidebar.css'; // Importando o arquivo de estilo CSS para a sidebar
 
 const Sidebar: React.FC = () => {
-    return (
-        <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: '280px' }}>
-            <ul className="nav nav-pills flex-column mb-auto">
-                <NavLink to={"/admin"} className="nav-img">
-                    <Image className="navbar-brand img-fluid " role="button"
-                           src={LogoHorizontal}/>
-                </NavLink>
-                <li className="nav-item">
-                    <NavLink to="/" className="nav-link active" aria-current="page">
-                        Inicial
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/boletins" className="nav-link">
-                        Boletins
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/criar-boletins" className="nav-link">
-                        Criar Boletins
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/alunos" className="nav-link">
-                        Alunos
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      {/* Sidebar - Brand */}
+      <li className="nav-item sidebar-brand d-flex align-items-center justify-content-center">
+        <NavLink to="/admin" className="nav-link">
+          <div className="sidebar-brand-icon">
+            <Image src={LogoHorizontalBranca} alt="GEPPA Logo" fluid />
+          </div>
+        </NavLink>
+      </li>
+
+      <li className="nav-item">
+        <NavLink to="/admin" className="nav-link">
+          <span>Inicial</span>
+        </NavLink>
+      </li>
+
+      <li className="nav-item">
+        <NavLink to="/admin/boletins" className="nav-link">
+          <span>Boletins</span>
+        </NavLink>
+      </li>
+
+      <li className="nav-item">
+        <NavLink to="/admin/criar-boletins" className="nav-link">
+          <i className="fas fa-edit"></i>
+          <span>Criar Boletins</span>
+        </NavLink>
+      </li>
+
+      <li className="nav-item">
+        <NavLink to="/admin/alunos" className="nav-link">
+          <i className="fas fa-users"></i>
+          <span>Alunos</span>
+        </NavLink>
+      </li>
+
+    </ul>
+  );
 };
 
 export default Sidebar;
