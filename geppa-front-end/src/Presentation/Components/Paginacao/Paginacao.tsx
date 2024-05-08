95a97048499cb313301afc3f7a13a7afd49db9e8
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-07 23:39:06 -0300

    refactor: Adicionado melhorias ao componente de paginação

--- geppa-front-end/src/Presentation/Components/Paginacao/Paginacao.tsx
+++ geppa-front-end/src/Presentation/Components/Paginacao/Paginacao.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Pagination} from 'react-bootstrap';
 import './paginacao.css';
+import cores from "../Utils/Cores.tsx";
 
 interface PaginationProps {
     currentPage: number;
@@ -8,23 +9,15 @@ interface PaginationProps {
     onPageChange: (page: number) => void;
 }
 
-const Paginacao: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
-    const range = (start: number, end: number) => Array.from({ length: end - start + 1 }, (_, i) => i + start);
+const Paginacao: React.FC<PaginationProps> = ({currentPage, totalPages, onPageChange}) => {
+    const range = (start: number, end: number) => Array.from({length: end - start + 1}, (_, i) => i + start);
 
     const getVisiblePages = () => {
         const visiblePages = [];
         const startPage = Math.max(0, currentPage - 2);
         const endPage = Math.min(totalPages - 1, currentPage + 2);
 
-        if (startPage > 0) {
-            visiblePages.push(0);
-        }
-
         visiblePages.push(...range(startPage, endPage));
-
-        if (endPage < totalPages - 1) {
-            visiblePages.push(totalPages - 1);
-        }
 
         return visiblePages;
     };
@@ -32,17 +25,26 @@ const Paginacao: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageC
     const visiblePages = getVisiblePages();
 
     return (
-        <Pagination className="justify-content-center">
-            <Pagination.First onClick={() => onPageChange(0)} disabled={currentPage === 0} />
-            <Pagination.Prev disabled={currentPage === 0} onClick={() => onPageChange(currentPage - 1)} />
-            {visiblePages.map((page, index) => (
-                <Pagination.Item key={index} active={page === currentPage} onClick={() => onPageChange(page)}>
-                    {page + 1}
-                </Pagination.Item>
-            ))}
-            <Pagination.Next disabled={currentPage === totalPages - 1} onClick={() => onPageChange(currentPage + 1)} />
-            <Pagination.Last onClick={() => onPageChange(totalPages - 1)} disabled={currentPage === totalPages - 1} />
-        </Pagination>
+        <>
+
+            <Pagination className="justify-content-center mb-1">
+                <Pagination.First onClick={() => onPageChange(0)} disabled={currentPage === 0}/>
+                <Pagination.Prev disabled={currentPage === 0} onClick={() => onPageChange(currentPage - 1)}/>
+
+                {visiblePages.map((page, index) => (
+                    <Pagination.Item key={index} active={page === currentPage} onClick={() => onPageChange(page)}>
+                        {page + 1}
+                    </Pagination.Item>
+                ))}
+                <Pagination.Next disabled={currentPage === totalPages - 1}
+                                 onClick={() => onPageChange(currentPage + 1)}/>
+                <Pagination.Last onClick={() => onPageChange(totalPages - 1)}
+                                 disabled={currentPage === totalPages - 1}/>
+            </Pagination>
+            <p className={'text-center fw-semibold fs-6'} style={{color: cores.marromEscuro}}>
+                Página {currentPage + 1} de {totalPages}
+            </p>
+        </>
     );
 };
 export default Paginacao;
