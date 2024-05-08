e7bbdc2b7cde91e0921fb978d0c817db74a893c3
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-07 22:41:08 -0300

    feat: implementado filtro de busca de tag por nome

--- geppa-front-end/src/Presentation/Pages/AdminUser/Tags/ListagemTags.tsx
+++ geppa-front-end/src/Presentation/Pages/AdminUser/Tags/ListagemTags.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {Button, Form, Table, InputGroup, Col} from 'react-bootstrap';
 import {Section} from "@react-email/components";
 import cores from "../../../Components/Utils/Cores.tsx";
@@ -8,6 +8,7 @@ import AxiosClient from "../../../../Data/Services/AxiosClient.ts";
 import Paginacao from "../../../Components/Paginacao/Paginacao.tsx";
 
 const ListagemTags: React.FC = () => {
+    const filterInputRef = useRef<HTMLInputElement>(null);
     const [tags, setTags] = useState<Tag[]>([]);
     const [filter, setFilter] = useState('');
     const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
@@ -16,21 +17,30 @@ const ListagemTags: React.FC = () => {
 
     useEffect(() => {
         const fetchTags = async () => {
-            const response = await AxiosClient(`/tags?page=${currentPage}&size=8`)
+            let response;
+            if (filter) {
+                response = await AxiosClient(`/tags/${filter}`);
+                setTotalPages(1);
+            } else {
+                response = await AxiosClient(`/tags?page=${currentPage}&size=8`);
+                setTotalPages(response.data.totalPaginas);
+            }
             setTags(response.data.dados);
-            setTotalPages(response.data.totalPaginas);
-            setCurrentPage(response.data.paginaAtual);
+            setCurrentPage(response.data.paginaAtual || 0);
         };
-
         fetchTags();
-    }, [currentPage]);
+    }, [currentPage, filter]);
 
-    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFilter(e.target.value);
+    const clearFilter = () => {
+        setFilter('');
+        if (filterInputRef.current) {
+            filterInputRef.current.value = '';
+        }
     };
 
+
     const filteredTags = tags.filter(tags =>
-        tags.nome.toLowerCase().includes(filter.toLowerCase())
+        tags.nome.includes(filter.toLowerCase())
     );
 
     const handleEdit = (tags: Tag) => {
@@ -62,13 +72,21 @@ const ListagemTags: React.FC = () => {
                     <Col md={8} className={"p-0"}>
                         <InputGroup className="mb-3 px-5">
                             <Form.Control
-                                placeholder="Buscar por nome"
-                                aria-label="Buscar por nome"
+                                placeholder="Encontre a tag por nome"
+                                aria-label="Encontre a tag por nome"
                                 aria-describedby="basic-addon2"
+                                ref={filterInputRef}
                             />
-                            <Button variant="outline-secondary" id="button-addon2">
-                                Filtrar
+                            <Button variant="outline-secondary" onClick={clearFilter}>
+                                <i className="ri-filter-off-line"></i>
                             </Button>
+                            <Button variant="outline-secondary" id="button-addon2" onClick={() => {
+                                setFilter(filterInputRef.current?.value || '');
+                            }}>
+                                Buscar
+                            </Button>
+
+
                         </InputGroup>
 
                         <CadastroTag buttonText={"Nova Tag"} iconClass={"ri-add-line"}/>
@@ -82,22 +100,27 @@ const ListagemTags: React.FC = () => {
                             </tr>
                             </thead>
                             <tbody>
-                            {filteredTags.map((tags, index) => (
-                                <tr key={tags.id}>
-                                    <td className={"align-middle"}>{index + 1}</td>
-                                    <td className={"align-middle"}>{tags.nome}</td>
-                                    <td className="text-center align-middle">
-                                        <Button variant="primary" onClick={() => handleEdit(tags)}>
-                                            Editar
-                                            <i className=" fw- ps-1 ri-pencil-fill"></i>
-                                        </Button>
-                                        <Button variant="danger" onClick={() => handleDelete(tags)}>
-                                            Excluir
-                                            <i className="ps-1 ri-delete-bin-line"></i>
-                                        </Button>
-                                    </td>
+                            {filteredTags.length > 0 ? (
+                                filteredTags.map((tags, index) => (
+                                    <tr key={tags.id}>
+                                        <td className={"align-middle"}>{index + 1}</td>
+                                        <td className={"align-middle"}>{tags.nome}</td>
+                                        <td className="text-center align-middle">
+                                            <Button variant="primary" onClick={() => handleEdit(tags)}>
+                                                Editar
+                                                <i className=" fw- ps-1 ri-pencil-fill"></i>
+                                            </Button>
+                                            <Button variant="danger" onClick={() => handleDelete(tags)}>
+                                                Excluir
+                                                <i className="ps-1 ri-delete-bin-line"></i>
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))) : (
+                                <tr>
+                                    <td colSpan={3} className="text-center">Nenhuma tag encontrada.</td>
                                 </tr>
-                            ))}
+                            )}
                             </tbody>
                         </Table>
                     </Col>
