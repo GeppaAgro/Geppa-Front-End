869721e25d6503a1b8c8eae67d55b2d41d77ba74
FellipeGodoi <115329585+FellipeGodoi@users.noreply.github.com>
2024-06-07 15:32:55 -0300

    fix: Ajuste de layout na criacao e edicao de boletim

--- geppa-front-end/src/Presentation/Components/Modais/ComponentesModal/ListaDeIndicadores.tsx
+++ geppa-front-end/src/Presentation/Components/Modais/ComponentesModal/ListaDeIndicadores.tsx
@@ -52,14 +52,14 @@ const ListaDeIndicadores: React.FC<{ indicadoresIniciais?: Indicador[], onUpdate
     };
 
     return (
-        <>
+        <div className='mx-md-5 '>
             <ModalIndicadores
                 abrir={modalIndicadores.show}
                 fechar={closeModalIndicadores}
                 salvar={(indicador: Indicador) => salvarIndicador(indicador, modalIndicadores.editIndex)}
                 indicador={modalIndicadores.editIndex !== null ? indicadores[modalIndicadores.editIndex] : undefined}/>
 
-            <div className="d-flex justify-content-between align-items-center ps-sm-5">
+            <div className="d-flex justify-content-between align-items-center">
                 <span className="fw-bold fs-5">Indicadores</span>
                 <button onClick={openModalIndicadores} className="btn-adicionar-indicador fw-bold fs-6 py-2 px-3 rounded">Adicionar Indicador
                 </button>
@@ -100,7 +100,7 @@ const ListaDeIndicadores: React.FC<{ indicadoresIniciais?: Indicador[], onUpdate
                 ))}
                 </tbody>
             </Table>
-        </>
+        </div>
 
     );
 }
